3b0fa0859c38434b4a864178325e95cf3d088439
Yusuf <yusufh97@live.com>
2018-04-10 18:19:44 -0700

    string

---
+++ String-compare/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 16.3 Write an app that uses string method CompareTo to compare two strings input
+  by the user. Output wether the first string is less than, equal to or greater
+  than the second.
+ */
+
+using System;
+
+namespace String_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            string str2;
+
+            Console.WriteLine("***The program will comapare two strings input*** \n");
+
+            Console.WriteLine("Enter first string: ");
+            str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second string: ");
+            str2 = Console.ReadLine();
+
+            if (str1.CompareTo(str2) < 0)
+            {
+                Console.WriteLine("the first string is less than second string");
+                Console.ReadLine();
+            }
+            else if (str1.CompareTo(str2) > 0)
+            {
+                Console.WriteLine("the first string is more than second string");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("the strings are equal");
+                Console.ReadLine();
+            }
+        }
+    }
+}
